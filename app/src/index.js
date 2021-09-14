c4e55fe0c8db3b0cf728ad32679bc6be57227146
iAmAsval <valentinbaudry64@gmail.com>
2021-09-14 22:06:08 +0200

    test

--- app/src/index.js
+++ app/src/index.js
@@ -9,21 +9,22 @@ ReactDOM.render(
 		<HelmetProvider>
 			<Helmet>
 				<meta name='theme-color' content='#D7354E' />
-				<meta name='description' content="Software for exploring Unreal Engine 4-5 games' files" />
-				<meta name='description' content="Software for exploring Unreal Engine 4-5 games' files" data-react-helmet='true' />
-				<meta name='og:url' content='' />
-				<meta name='og:site_name' content='' />
-				<meta name='og:locale' content='' />
-				<meta name='og:type' content='' />
-				<meta name='og:title' content='' />
-				<meta name='og:description' content='' />
-				<meta name='og:image' content='' />
-				<meta name='og:image:alt' content='' />
-				<meta name='twitter:card' content='' />
-				<meta name='twitter:title' content='' />
-				<meta name='twitter:description' content='' />
-				<meta name='twitter:image' content='' />
-				<meta name='twitter:image:alt' content='' />
+				<meta name='description' content="FModel is a beginner-friendly and open-source software for data-mining games made with Unreal Engine." />
+				<meta name='description' content="FModel is a beginner-friendly and open-source software for data-mining games made with Unreal Engine." data-react-helmet='true' />
+				<meta name='og:url' content='https://fmodel.app/' />
+				<meta name='og:site_name' content='FModel' />
+				<meta name='og:locale' content='en_US' />
+				<meta name='og:type' content='website' />
+				<meta name='og:title' content='FModel' />
+				<meta name='og:description' content="FModel is a beginner-friendly and open-source software for data-mining games made with Unreal Engine." />
+				<meta name='og:image' content='https://cdn.fmodel.app/i/png/fmodel_banner.png' />
+				<meta name='og:image:alt' content='FModel Banner Art' />
+				<meta name='twitter:card' content='summary' />
+				<meta name='twitter:site' content='@FModelApp' />
+				<meta name='twitter:title' content='FModel' />
+				<meta name='twitter:description' content="FModel is a beginner-friendly and open-source software for data-mining games made with Unreal Engine." />
+				<meta name='twitter:image' content='https://cdn.fmodel.app/i/png/fmodel_banner.png' />
+				<meta name='twitter:image:alt' content='FModel Banner Art' />
 				<title>FModel</title>
 			</Helmet>
 			<App />
