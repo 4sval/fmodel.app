c60fa548ad59f5e0b34915c2c2eef26d30d79602
iAmAsval <valentinbaudry64@gmail.com>
2021-11-05 00:23:31 +0100

    prout

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     purge: {
         content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
         options: {
-            whitelistPatterns: [
+            safelist: [
                 /.*orange-.*/,
                 /.*amber-.*/,
                 /.*lime-.*/,
